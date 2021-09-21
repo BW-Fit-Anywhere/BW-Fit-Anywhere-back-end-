7a8ad123cc27092b3d5dd11b43f16312776cea37
Michael Guerrero <32268444+MikeLikesCode@users.noreply.github.com>
2021-09-20 20:56:42 -0400

    Adding foreign key support to the knex config file

--- knexfile.js
+++ knexfile.js
@@ -21,6 +21,7 @@ const sharedConfig = {
   client: 'pg',
   migrations: { directory: './api/data/migrations' },
   seeds: { directory: './api/data/seeds' },
+  pool: {afterCreate: (conn, done) => {conn.run('PRAGMA foreign_keys = ON', done)}}
 }
 
 module.exports = {
