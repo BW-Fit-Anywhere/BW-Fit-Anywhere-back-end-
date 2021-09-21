da9f6dc5f451936cacb0cd8d9454d79656f27974
Michael Guerrero <32268444+MikeLikesCode@users.noreply.github.com>
2021-09-20 20:53:51 -0400

    Quick fix for migration file

--- api/data/migrations/20210919150216_first-migration.js
+++ api/data/migrations/20210919150216_first-migration.js
@@ -1,26 +1,25 @@
-exports.up = async (knex) => {
-  await knex.schema
-    .createTable("roles", (roles) => {
-      roles.increments("role_id");
-      roles.string("role_name", 150).notNullable();
+exports.up = function (knex) {
+  return knex.schema
+    .createTable('roles', roles => {
+      roles.increments('role_id')
+      roles.string('role_name', 32).notNullable().unique()
     })
-    .createTable("users", (users) => {
-      users.increments("user_id");
-      users.string("username", 200).notNullable();
-      users.string("password", 200).notNullable();
-      users.timestamps(false, true);
-      users
-        .integer("role_id")
+    .createTable('users', users => {
+      users.increments('user_id')
+      users.string('username', 200).notNullable().unique()
+      users.string('password', 200).notNullable()
+      users.integer('role_id')
         .unsigned()
         .notNullable()
-        .references("role_id")
-        .inTable("roles")
-        .onUpdate("RESTRICT")
-        .onDelete("RESTRICT");
-    });
-};
+        .references('role_id')
+        .inTable('roles')
+        .onUpdate('RESTRICT')
+        .onDelete('RESTRICT')
+    })
+}
 
-exports.down = async (knex) => {
-  await knex.schema.dropTableIfExists("users");
-  await knex.schema.dropTableIfExists("roles");
-};
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists('users')
+    .dropTableIfExists('roles')
+}
