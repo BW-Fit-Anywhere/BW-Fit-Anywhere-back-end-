e4308f1fa43b8ab0281ab44f37364adad705b48a
Michael <maikurusama@gmail.com>
2021-09-21 12:16:35 -0400

    Adding dummy data for classes

--- api/data/seeds/01-data.js
+++ api/data/seeds/01-data.js
@@ -1,6 +1,7 @@
 exports.seed = async function (knex) {
   await knex.raw('TRUNCATE TABLE roles CASCADE')
   await knex('users').truncate()
+  await knex('classes').truncate()
   await knex('roles').insert([
     { role_name: 'administator' },
     { role_name: 'client' },
@@ -17,5 +18,16 @@ exports.seed = async function (knex) {
       password: '$2a$10$dFwWjD8hi8K2I9/Y65MWi.WU0qn9eAVaiBoRSShTvuJVGw8XpsCiq', // password "1234"
       role_id: 3,
     },
+  ])
+  await knex('classes').insert([
+    {
+      name: 'testing',
+      type: 'something',
+      start_time: '3pm',
+      intensity_level: 'hard',
+      location: 'New York',
+      number_registered: 12,
+      max_class_size: 14
+    }
   ])
 }
