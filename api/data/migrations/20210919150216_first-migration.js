190e690b91d83a1fbdaf89a30d3670e851e96a16
Michael Guerrero <32268444+MikeLikesCode@users.noreply.github.com>
2021-09-20 20:39:18 -0400

    Adding an extra table for roles

--- api/data/migrations/20210919150216_first-migration.js
+++ api/data/migrations/20210919150216_first-migration.js
@@ -1,13 +1,26 @@
 exports.up = async (knex) => {
-    await knex.schema
-      .createTable('users', (users) => {
-        users.increments('user_id')
-        users.string('username', 200).notNullable()
-        users.string('password', 200).notNullable()
-        users.timestamps(false, true)
-      })
-  }
-  
-  exports.down = async (knex) => {
-    await knex.schema.dropTableIfExists('users')
-  }
+  await knex.schema
+    .createTable("roles", (roles) => {
+      roles.increments("role_id");
+      roles.string("role_name", 150).notNullable();
+    })
+    .createTable("users", (users) => {
+      users.increments("user_id");
+      users.string("username", 200).notNullable();
+      users.string("password", 200).notNullable();
+      users.timestamps(false, true);
+      users
+        .integer("role_id")
+        .unsigned()
+        .notNullable()
+        .references("role_id")
+        .inTable("roles")
+        .onUpdate("RESTRICT")
+        .onDelete("RESTRICT");
+    });
+};
+
+exports.down = async (knex) => {
+  await knex.schema.dropTableIfExists("users");
+  await knex.schema.dropTableIfExists("roles");
+};
