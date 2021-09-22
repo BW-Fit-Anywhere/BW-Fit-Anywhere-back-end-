bc64f7c59297bd4db6b9ef03867a81e006a21931
Michael <maikurusama@gmail.com>
2021-09-22 10:40:48 -0400

    Testing the first get route to see if it works. The route overall works now

--- api/classes/classes-router.js
+++ api/classes/classes-router.js
@@ -2,19 +2,19 @@ const router = require('express').Router()
 const md = require('./classes-middleware')
 const Classes = require('./classes-model')
 
-router.get('/', (req,res) => {
-  res.json('ok!');
-})
-
-
-// router.get('/', async (req, res, next) => {
-//   try {
-//     const classes = await Classes.getAll()
-//     res.status(200).json(classes)
-//   } catch (err) {
-//     next(err)
-//   }
+// router.get('/', (req,res) => {
+//   res.json('ok!');
 // })
+
+
+router.get('/', async (req, res, next) => {
+  try {
+    const classes = await Classes.getAll()
+    res.status(200).json(classes)
+  } catch (err) {
+    next(err)
+  }
+})
 
 // router.get('/:id', md.checkAccountId, async (req, res, next) => {
 //   res.json(req.classes)
