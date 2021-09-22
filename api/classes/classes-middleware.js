7d563724cbc1cdc1abbc68975cb5237a7840e821
Michael <maikurusama@gmail.com>
2021-09-22 10:15:36 -0400

    Fixing path for classes middleware

--- api/classes/classes-middleware.js
+++ api/classes/classes-middleware.js
@@ -1,5 +1,5 @@
 const Classes = require('./classes-model')
-const db = require('../../data/db-config')
+const db = require('../data/db-config')
 
 exports.checkAccountPayload = (req, res, next) => {
   const error = { status: 400 }
