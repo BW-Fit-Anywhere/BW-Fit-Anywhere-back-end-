df69bd3445de361818cacd241cf886a3e07b1724
Michael <maikurusama@gmail.com>
2021-09-22 10:36:26 -0400

    Error still persisting

--- api/classes/classes-router.js
+++ api/classes/classes-router.js
@@ -2,63 +2,67 @@ const router = require('express').Router()
 const md = require('./classes-middleware')
 const Classes = require('./classes-model')
 
-
-router.get('/', async (req, res, next) => {
-  try {
-    const classes = await Classes.getAll()
-    res.status(200).json(classes)
-  } catch (err) {
-    next(err)
-  }
+router.get('/', (req,res) => {
+  res.json('ok!');
 })
 
-router.get('/:id', md.checkAccountId, async (req, res, next) => {
-  res.json(req.classes)
-})
 
-router.post(
-  '/',
-  md.checkAccountPayload,
-  md.checkAccountNameUnique,
-  async (req, res, next) => {
-    try {
-      const newClasses = await Classes.create({
-        name: req.body.name.trim(),
-        type: req.body.type,
-        start_time: req.body.start_time,
-        intensity_level: req.body.intensity_level,
-        location: req.body.location,
-        number_registered: req.body.number_registeted,
-        max_class_size: req.body.max_class_size,
+// router.get('/', async (req, res, next) => {
+//   try {
+//     const classes = await Classes.getAll()
+//     res.status(200).json(classes)
+//   } catch (err) {
+//     next(err)
+//   }
+// })
+
+// router.get('/:id', md.checkAccountId, async (req, res, next) => {
+//   res.json(req.classes)
+// })
+
+// router.post(
+//   '/',
+//   md.checkAccountPayload,
+//   md.checkAccountNameUnique,
+//   async (req, res, next) => {
+//     try {
+//       const newClasses = await Classes.create({
+//         name: req.body.name.trim(),
+//         type: req.body.type,
+//         start_time: req.body.start_time,
+//         intensity_level: req.body.intensity_level,
+//         location: req.body.location,
+//         number_registered: req.body.number_registeted,
+//         max_class_size: req.body.max_class_size,
 
 
-      })
-      res.status(201).json(newClasses)
-    } catch (err) {
-      next(err)
-    }
-  })
+//       })
+//       res.status(201).json(newClasses)
+//     } catch (err) {
+//       next(err)
+//     }
+//   })
 
-router.put(
-  '/:id',
-  md.checkAccountId,
-  md.checkAccountPayload,
-  async (req, res, next) => {
-    try {
-      const updated = await Classes.updateById(req.params.id, req.body)
-      res.json(updated)
-    } catch (err) {
-      next(err)
-    }
-  });
+// router.put(
+//   '/:id',
+//   md.checkAccountId,
+//   md.checkAccountPayload,
+//   async (req, res, next) => {
+//     try {
+//       const updated = await Classes.updateById(req.params.id, req.body)
+//       res.json(updated)
+//     } catch (err) {
+//       next(err)
+//     }
+//   });
 
-router.delete('/:id', md.checkAccountId, async (req, res, next) => {
-  try {
-    await Classes.deleteById(req.params.id)
-    res.json(req.classes)
-  } catch (err) {
-    next(err)
-  }
-})
+// router.delete('/:id', md.checkAccountId, async (req, res, next) => {
+//   try {
+//     await Classes.deleteById(req.params.id)
+//     res.json(req.classes)
+//   } catch (err) {
+//     next(err)
+//   }
+// })
 
 module.exports = router;
