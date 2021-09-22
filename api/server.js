59e82fc4040feff54641683e8c43e749f3c6a257
Michael <maikurusama@gmail.com>
2021-09-22 10:29:56 -0400

    Testing to see if it's a caching issue

--- api/server.js
+++ api/server.js
@@ -14,7 +14,7 @@ server.use(cors());
 
 server.use('/api/auth', authRouter);
 server.use('/api/users', usersRouter);
-server.use('/api/classes', classesRouter)
+server.use('/api/classes', usersRouter);
 
 
 server.get('/', (req,res) => {
