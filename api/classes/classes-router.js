493bcfe56e0ab013300bf56427036753b4eddd65
Michael <maikurusama@gmail.com>
2021-09-22 10:59:25 -0400

    Adding back rest of the routes

--- api/classes/classes-router.js
+++ api/classes/classes-router.js
@@ -2,11 +2,6 @@ const router = require('express').Router()
 const { checkAccountId, checkAccountNameUnique, checkAccountPayload } = require('./classes-middleware')
 const Classes = require('./classes-model')
 
-// router.get('/', (req,res) => {
-//   res.json('ok!');
-// })
-
-
 router.get('/', async (req, res, next) => {
   try {
     const classes = await Classes.getAll()
@@ -25,49 +20,49 @@ router.get('/:id', checkAccountId , async (req, res, next) => {
   }
 })
 
-// router.post(
-//   '/',
-//   md.checkAccountPayload,
-//   md.checkAccountNameUnique,
-//   async (req, res, next) => {
-//     try {
-//       const newClasses = await Classes.create({
-//         name: req.body.name.trim(),
-//         type: req.body.type,
-//         start_time: req.body.start_time,
-//         intensity_level: req.body.intensity_level,
-//         location: req.body.location,
-//         number_registered: req.body.number_registeted,
-//         max_class_size: req.body.max_class_size,
+router.post(
+  '/',
+  checkAccountPayload,
+  checkAccountNameUnique,
+  async (req, res, next) => {
+    try {
+      const newClasses = await Classes.create({
+        name: req.body.name.trim(),
+        type: req.body.type,
+        start_time: req.body.start_time,
+        intensity_level: req.body.intensity_level,
+        location: req.body.location,
+        number_registered: req.body.number_registeted,
+        max_class_size: req.body.max_class_size,
 
 
-//       })
-//       res.status(201).json(newClasses)
-//     } catch (err) {
-//       next(err)
-//     }
-//   })
+      })
+      res.status(201).json(newClasses)
+    } catch (err) {
+      next(err)
+    }
+  })
 
-// router.put(
-//   '/:id',
-//   md.checkAccountId,
-//   md.checkAccountPayload,
-//   async (req, res, next) => {
-//     try {
-//       const updated = await Classes.updateById(req.params.id, req.body)
-//       res.json(updated)
-//     } catch (err) {
-//       next(err)
-//     }
-//   });
+router.put(
+  '/:id',
+  checkAccountId,
+  checkAccountPayload,
+  async (req, res, next) => {
+    try {
+      const updated = await Classes.updateById(req.params.id, req.body)
+      res.json(updated)
+    } catch (err) {
+      next(err)
+    }
+  });
 
-// router.delete('/:id', md.checkAccountId, async (req, res, next) => {
-//   try {
-//     await Classes.deleteById(req.params.id)
-//     res.json(req.classes)
-//   } catch (err) {
-//     next(err)
-//   }
-// })
+router.delete('/:id', checkAccountId, async (req, res, next) => {
+  try {
+    await Classes.deleteById(req.params.id)
+    res.json(req.classes)
+  } catch (err) {
+    next(err)
+  }
+})
 
 module.exports = router;
